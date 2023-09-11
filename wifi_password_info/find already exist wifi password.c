d69b1b356ae72b40ee8ecdfb0589bdffc1100a0a
giorgos ilia <giorkos313@gmail.com>
2023-09-11 20:40:48 +0300

    Update find already exist wifi password.c
        Removed unnecessary header files (simpio.h, genlib.h) and variables (response).
    
        Removed the char find return type since the function doesn't return a value.
    
        Simplified the main function and added a clear message at the beginning.
    
        Combined the sprintf commands to simplify code and reduce redundancy.
    
        Added error handling for cases where password retrieval fails.
    
        Improved user prompts and messages for a better user experience.

--- wifi_password_info/find already exist wifi password.c
+++ wifi_password_info/find already exist wifi password.c
@@ -1,49 +1,52 @@
-//attempt1
-#include "simpio.h"
-#include "stdio.h"
-#include "genlib.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-
-
-
-char find(char cmd[],char name[]);
+void find(char name[]);
 
 int main()
 {
-    char cmd[512];
     char name[100];
 
+    printf("WiFi Password Finder\n");
 
-    char response[3];
-    find(cmd,name);
+    find(name);
 
+    printf("\nYour WiFi information has been saved!\n");
 
-printf("\nYour WiFi information saved !\n");
-
-system("pause");
+    // Pause before exiting
+    system("pause");
+    return 0;
 }
 
-char find(char cmd[],char name[])
+void find(char name[])
 {
+    char cmd[512];
 
+    // Display available WiFi profiles
     system("netsh wlan show profile");
-    printf("please enter one wifi name from above : ");
+
+    printf("Please enter the name of the WiFi network: ");
     gets(name);
 
+    while (strlen(name) == 0)
+    {
+        printf("\nPlease provide a valid network name: ");
+        gets(name);
+    }
 
-   while (strlen(name) == 0)
-        {
-            printf("\nPlease give a name: ");
-            gets(name);
-        }
-   if(strlen(name) != 0)
-   {
-        sprintf(cmd,"NETSH WLAN SHOW PROFILE \"%s\" KEY=CLEAR ",name);
-        system(cmd);
-        sprintf(cmd,"NETSH WLAN SHOW PROFILE \"%s\" KEY=CLEAR |clip",name);
-        system(cmd);
-        system("powershell -command Get-Clipboard > password_info.txt");
-   }
+    sprintf(cmd, "NETSH WLAN SHOW PROFILE \"%s\" KEY=CLEAR | clip", name);
+    system(cmd);
 
+    printf("\nRetrieving WiFi information for '%s'...\n", name);
+
+    // Check if the clipboard contains data
+    if (system("powershell -command Get-Clipboard > password_info.txt") == 0)
+    {
+        printf("Password information saved to 'password_info.txt'.\n");
+    }
+    else
+    {
+        printf("Failed to retrieve password information.\n");
+    }
 }
